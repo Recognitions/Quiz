05b1b01bd4cee4284284355fde05121b49947b12
Ronix666 <2020infor13@gmail.com>
2022-08-29 23:35:20 -0300

    codigo melhorado

--- perguntas.js
+++ perguntas.js
@@ -1,98 +1,45 @@
-export function questions(){
-    document.querySelector("main").innerHTML=`
-        <label></label>
-        <div id="inputs">
-
-        </div>
-    `
-    const array = [
-        {
-            id: 1,
-            title: "Questão de fato histórico.",
-            text: "Quem descobriu o Brasil?",
-            alt: [
-                {
-                    quote: "A",
-                    text: "Eu",
-                    value: false
-                },
-                {
-                    quote: "B",
-                    text: "Sua mãe",
-                    value: false
-                },
-                {
-                    quote: "C",
-                    text: "Pedro",
-                    value: true
-                }
-            ]
-        },
-        {
-            id: 2,
-            title: "Questão de lógica",
-            text: "Quanto é dois mais dois dividido por dois?",
-            alt: [
-                {
-                    quote: "A",
-                    text: "1",
-                    value: false
-                },
-                {
-                    quote: "B",
-                    text: "3",
-                    value: true
-                },
-                {
-                    quote: "C",
-                    text: "4",
-                    value: false
-                }
-            ]
-        }
-    ]
-    let random = Math.floor(Math.random()*array.length)
-    console.log(`
-            Tipo: ${array[random].title}
-        Pergunta: ${array[random].text}
-    `)
-    for (let index = 0; index < array[random].alt.length; index++) {
-        const element = array[random].alt[index];
-        console.log(`
-            (${element.quote}) ${element.text}
-        `)
-        
-        const inputs = document.querySelector("#inputs")
-        const div = document.createElement("div")
-        div.innerHTML=`
-            <button id="A${element.quote}">${element.text}</button>
-        `
-        inputs.appendChild(div)
-        document.querySelector(`#A${element.quote}`).addEventListener("click",()=>{
-            document.querySelector(`#A${element.quote}`).disabled
-            if(element.value==true){
-                let time = 3
-                for (let t = 0; t <= time; t++) {
-                    setTimeout(()=>{
-                        document.querySelector("h1").innerText="Você acertou! próxima pergunta em: "+((time)-t)
-                        if(t==(time-1)){
-                            questions()
-                        }
-                    },1000*t)
-                }
-            }else{
-                console.log("Errou")
+export const array = [
+    {
+        title: "Questão de fato histórico.",
+        text: "Quem descobriu o Brasil?",
+        alt: [
+            {
+                quote: "A",
+                text: "Eu",
+                value: false
+            },
+            {
+                quote: "B",
+                text: "Sua mãe",
+                value: false
+            },
+            {
+                quote: "C",
+                text: "Pedro",
+                value: true
             }
-        })
-        
+        ]
+    },
+    {
+        title: "Questão de lógica",
+        text: "Quanto é dois mais dois dividido por dois?",
+        alt: [
+            {
+                quote: "A",
+                text: "1",
+                value: false
+            },
+            {
+                quote: "B",
+                text: "3",
+                value: true
+            },
+            {
+                quote: "C",
+                text: "4",
+                value: false
+            }
+        ]
     }
-    let myAwnser = array[random].alt[2]
-    console.log(`Você escolheu a alternativa: ${myAwnser.quote}`)
-    if(myAwnser.value==false){
-        console.log("Resultado: Errado!")
-    }else{
-        console.log("Resultado: Correto!")
-    }
-    array.splice(random,1)
-}
-
+    
+]
--- script.js
+++ script.js
@@ -1,4 +1,64 @@
-import { questions } from './perguntas.js';
+import { array } from './perguntas.js';
 
 
+function clear(){
+    document.querySelector("main").innerHTML=`
+    <h1></h1>
+    <label></label>
+    <div id="inputs">
+
+    </div>
+    `
+}
+function lose(state){
+    const lose = document.getElementById("lose")
+    if(state=="show"){
+        lose.style.display="flex"
+    }else if(state=="hide"){
+        lose.style.display="none"
+    }
+}
+function win(state){
+    const win = document.getElementById("win")
+    if(state=="show"){
+        win.style.display="flex"
+    }else if(state=="hide"){
+        win.style.display="none"
+    }
+}
+
+function questions(){
+    if(array.length>0){
+        let random = Math.floor(Math.random()*array.length)
+        document.querySelector(`main h1`).innerText=array[random].title
+        document.querySelector(`main label`).innerText=array[random].text
+        for (let index = 0; index < array[random].alt.length; index++) {
+            const element = array[random].alt[index];
+            const inputs = document.querySelector("#inputs")
+            const div = document.createElement("div")
+            div.innerHTML=`
+            <button id="A${element.quote}">${element.text}</button>
+            `
+            inputs.appendChild(div)
+            document.querySelector(`#A${element.quote}`).addEventListener("click",()=>{
+                
+                document.querySelector(`#A${element.quote}`).disabled
+                if(element.value==true){
+                    clear()
+                    questions()
+                }else{
+                    clear()
+                    lose("show")
+                }
+            })
+        }
+        array.splice(random,1)
+    }else{
+        win("show")
+    }
+}
 questions()
+
+document.getElementById("reset").addEventListener("click",()=>{
+    document.location.href="./"
+})
