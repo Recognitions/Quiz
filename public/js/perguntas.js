decc1f7d829e830294df00e7c739013f3ae3bc1b
Ronix666 <2020infor13@gmail.com>
2022-09-04 12:45:07 -0300

    perguntas

--- public/js/perguntas.js
+++ public/js/perguntas.js
@@ -1,66 +1,148 @@
 export const array = [
     {
-        title: "Questão de fato histórico.",
-        text: "Quem descobriu o Brasil?",
+        title: "Questão de Matemática.",
+        text: "Se 3 gatos matam 3 ratos em 3 minutos, quanto tempo levarão 100 gatos para matar 100 ratos?",
         alt: [
             {
                 quote: "A",
-                text: "Eu",
+                text: "1 minuto",
                 value: false
             },
             {
                 quote: "B",
-                text: "Sua mãe",
-                value: false
-            },
-            {
-                quote: "C",
-                text: "Pedro",
-                value: true
-            }
-        ]
-    },
-    {
-        title: "Questão de lógica",
-        text: "Quanto é dois mais dois dividido por dois?",
-        alt: [
-            {
-                quote: "A",
-                text: "1",
-                value: false
-            },
-            {
-                quote: "B",
-                text: "3",
+                text: "3 minutos",
                 value: true
             },
             {
                 quote: "C",
-                text: "4",
+                text: "30 minutos",
+                value: false
+            },
+          {
+                quote: "D",
+                text: "100 minutos",
                 value: false
             }
         ]
     },
     {
-        title: "Questão de lógica",
-        text: "Qual o próximo número da sequência {1, 3, 5, 7,...}",
+        title: "Questão de Conhecimentos Gerais",
+        text: "O poder executivo no Brasil tem como principal representante o:",
         alt: [
             {
                 quote: "A",
-                text: "10",
+                text: "Vereador",
                 value: false
             },
             {
                 quote: "B",
-                text: "8",
+                text: "Prefeito",
                 value: false
             },
             {
                 quote: "C",
-                text: "9",
+                text: "Governador",
+                value: false
+            },
+          {
+                quote: "D",
+                text: "Deputado Estadual",
+                value: false
+            },
+          {
+                quote: "E",
+                text: "Presidente da República",
                 value: true
             }
         ]
-    }
+    },
+    {
+        title: "Questão de Geografia",
+        text: "A única capital brasileira que não pertence à Região Nordeste é:",
+        alt: [
+            {
+                quote: "A",
+                text: "Natal",
+                value: false
+            },
+            {
+                quote: "B",
+                text: "Teresina",
+                value: false
+            },
+            {
+                quote: "C",
+                text: "São Luís",
+                value: false
+            },
+          {
+                quote: "D",
+                text: "Vitória",
+                value: true
+            },
+                  {
+                quote: "E",
+                text: "Maceió",
+                value: false
+            }
+        ]
+    },
+    {
+        title: "Questão de Filosofia",
+        text: "René Descartes desenvolveu um método filosófico baseado na dúvida para encontrar uma certeza na qual possa fundamentar o conhecimento seguro. Essa certeza fundamental de Descartes é chamada de cogito e sua formulação principal diz:",
+        alt: [
+            {
+                quote: "A",
+                text: "Penso, logo existo",
+                value: true
+            },
+            {
+                quote: "B",
+                text: "Deus sive natura",
+                value: false
+            },
+            {
+                quote: "C",
+                text: "Conhece-te a ti mesmo",
+                value: false
+            },
+            {
+                quote: "D",
+                text: "Só sei que nada sei",
+                value: false
+            }
+        ]
+    },
+  {
+        title: "Questão de Conhecimentos Gerais",
+        text: "Se você for a um empório, estará indo em um...",
+        alt: [
+            {
+                quote: "A",
+                text: "ponto turístico importante de uma cidade",
+                value: false
+            },
+            {
+                quote: "B",
+                text: "edifício onde se reúnem os fiéis para exercer o seu culto",
+                value: false
+            },
+            {
+                quote: "C",
+                text: "estabelecimento comercial que vende vários tipos de mercadorias",
+                value: true
+            },
+          {
+                quote: "D",
+                text: "terreno arborizado, destinado à recreação",
+                value: false
+            },
+          {
+                quote: "E",
+                text: "passeio de estudo ou de observação",
+                value: false
+            }
+        ]
+    },
     
 ]
