17c417875f7d69cf61b4802ba73650de1abaea71
GeronimoNeto <102226234+GeronimoNeto@users.noreply.github.com>
2022-09-13 14:55:42 -0300

    admin

---
+++ public/js/login.js
@@ -0,0 +1,29 @@
+//console.log(md5("teste"))
+function render(){
+    const players = ((JSON.parse(localStorage.getItem("players"))).sort((a,b)=>{return b.points - a.points})).slice(0,10)
+    console.log(players)
+    players.forEach((player)=>{
+        const tbody = document.querySelector("tbody")
+        const tr = document.createElement("tr")
+        tr.innerHTML=`
+            <td>${player.name}</td>
+            <td>${player.points}</td>
+            <td>
+                <button>E</button>
+                <button>X</button>
+            </td>
+        `
+        tbody.appendChild(tr)
+    });
+}
+
+function login(){
+    const password = "698dc19d489c4e4db73e28a713eab07b"
+    if(md5(prompt("Password"))==password){
+        render()
+    }else{
+        login()
+    }
+}
+
+document.addEventListener("DOMContentLoaded",login)
