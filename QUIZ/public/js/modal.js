2f22897ccdb118e7b480065ca69327e648b39fcc
Carlos Daniel <87772189+devroseno@users.noreply.github.com>
2022-09-22 11:26:53 -0300

    Delete QUIZ directory

--- QUIZ/public/js/modal.js
+++
@@ -1,8 +0,0 @@
-export function modal(t,m){
-    const modal = document.querySelector(m)
-    if(t=="open"){
-        modal.classList.add("active")
-    }else if(t=="close"){
-        modal.classList.remove("active")
-    }
-}
