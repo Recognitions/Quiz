26579eaf47a298bf057242598d93a449c9f0e389
GeronimoNeto <102226234+GeronimoNeto@users.noreply.github.com>
2022-10-04 13:14:00 -0300

    .

--- public/js/perguntas.js
+++ public/js/perguntas.js
@@ -41,41 +41,6 @@ export const array = [
             },
             {
                 quote: "C",
-<<<<<<< HEAD
-                text: "Governador",
-                value: false
-            },
-            {
-                quote: "D",
-                text: "Presidente da República",
-                value: true
-            }
-        ]
-    },
-    {
-        title: "Questão de Geografia",
-        text: "A única capital brasileira que não pertence à Região Nordeste é:",
-        alt: [
-            {
-                quote: "A",
-                text: "Natal",
-                value: false
-            },
-            {
-                quote: "B",
-                text: "Teresina",
-                value: false
-            },
-            {
-                quote: "C",
-                text: "São Luís",
-                value: false
-            },
-            {
-                quote: "D",
-                text: "Vitória",
-                value: true
-=======
                 text: "Ansiosos, os candidatos aguardavam, em fila, o resultado do concurso.",
                 value: true
             },
@@ -83,7 +48,6 @@ export const array = [
                 quote: "D",
                 text: "Os candidatos ansiosos aguardavam o resultado do concurso, em fila.",
                 value: false
->>>>>>> 2810e0ed9b2c8821ea416d8f62715eb31cc981c1
             }
         ]
     },
@@ -134,11 +98,7 @@ export const array = [
             },
             {
                 quote: "D",
-<<<<<<< HEAD
-                text: "terreno arborizado, destinado à recreação",
-=======
                 text: "Carla tem 10, Paula tem 12, Leila tem 14 e Vivian tem 16.",
->>>>>>> 2810e0ed9b2c8821ea416d8f62715eb31cc981c1
                 value: false
             }
         ]
@@ -164,11 +124,7 @@ export const array = [
             },
             {
                 quote: "D",
-<<<<<<< HEAD
-                text: "Malta e Estados Unidos",
-=======
                 text: "0,2568",
->>>>>>> 2810e0ed9b2c8821ea416d8f62715eb31cc981c1
                 value: false
             }
         ]
@@ -194,11 +150,7 @@ export const array = [
             },
             {
                 quote: "D",
-<<<<<<< HEAD
-                text: "Castro Alves, Bento Teixeira e Manuel Botelho de Oliveira",
-=======
                 text: "145",
->>>>>>> 2810e0ed9b2c8821ea416d8f62715eb31cc981c1
                 value: false
             }
         ]
@@ -224,121 +176,9 @@ export const array = [
             },
             {
                 quote: "D",
-<<<<<<< HEAD
-                text: "Fabiano cortou o saco antes de cair",
-                value: false
-            }
-        ]
-    },
-    {
-        title: "Questão de Conhecimentos Gerais",
-        text: "Quais os planetas do sistema solar?",
-        alt: [
-            {
-                quote: "A",
-                text: "Terra, Vênus, Saturno, Urano, Júpiter, Marte, Netuno, Mercúrio",
-                value: true
-            },
-            {
-                quote: "B",
-                text: "Júpiter, Marte, Mercúrio, Netuno, Plutão, Saturno, Terra, Urano, Vênus",
-                value: false
-            },
-            {
-                quote: "C",
-                text: "Vênus, Saturno, Urano, Júpiter, Marte, Netuno, Mercúrio",
-                value: false
-            },
-            {
-                quote: "D",
-                text: "Júpiter, Marte, Mercúrio, Netuno, Plutão, Saturno, Sol, Terra, Urano, Vênus",
-                value: false
-            }
-        ]
-    },
-    {
-        title: "Questão de Literatura",
-        text: "Quais os principais heterônimos de Fernando Pessoa?",
-        alt: [
-            {
-                quote: "A",
-                text: "Alberto Caeiro, Ricardo Reis e Álvaro de Campos",
-                value: true
-            },
-            {
-                quote: "B",
-                text: "Ariano Suassuna, Raul Bopp e Quincas Borba",
-                value: false
-            },
-            {
-                quote: "C",
-                text: "Bento Teixeira, Ricardo Reis e Haroldo de Campos",
-                value: false
-            },
-            {
-                quote: "D",
-                text: "Alberto Caeiro, Ricardo Leite e Augusto de Campos",
-                value: false
-            }
-        ]
-    },
-    {
-        title: "Questão de Conhecimentos Gerais",
-        text: "Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?",
-        alt: [
-            {
-                quote: "A",
-                text: "Tem entre 2 a 4 litros. São retirados 450 mililitros",
-                value: false
-            },
-            {
-                quote: "B",
-                text: "Tem entre 4 a 6 litros. São retirados 450 mililitros",
-                value: true
-            },
-            {
-                quote: "C",
-                text: "Tem 10 litros. São retirados 2 litros",
-                value: false
-            },
-            {
-                quote: "D",
-                text: "Tem 7 litros. São retirados 1,5 litros",
-                value: false
-            }
-        ]
-    },
-    {
-        title: "Questão de conhecimentos gerais",
-        text: "De onde é a invenção do chuveiro elétrico?",
-        alt: [
-            {
-                quote: "A",
-                text: "França",
-                value: false
-            },
-            {
-                quote: "B",
-                text: "Inglaterra",
-                value: false
-            },
-            {
-                quote: "C",
-                text: "Brasil",
-                value: true
-            },
-            {
-                quote: "D",
-                text: "Austrália",
-                value: false
-            }
-        ]
-    }
-=======
                 text: "",
                 value: false
             }
         ]
     }, 
->>>>>>> 2810e0ed9b2c8821ea416d8f62715eb31cc981c1
 ]
